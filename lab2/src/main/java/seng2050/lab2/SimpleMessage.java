e3f5e17a06b0aa640579c26e771d6ffb8cfabb79
Josh <c3517809@uon.edu.au>
2026-02-06 12:49:27 +1100

    updated message

---
+++ lab2/src/main/java/seng2050/lab2/SimpleMessage.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+public class SimpleMessage extends HttpServlet {
+protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+throws ServletException, IOException {
+PrintWriter out = resp.getWriter();
+out.println("Welcome to the Website");
+}
+}
